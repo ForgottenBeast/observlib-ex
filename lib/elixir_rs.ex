c7058551a5fbd191d50454fd43c24a719e1a565b
fb <fb@dev.null>
2026-03-27 10:14:07 +0100

    initial elixir files

---
+++ lib/elixir_rs.ex
@@ -0,0 +1,18 @@
+defmodule ElixirRs do
+  @moduledoc """
+  Documentation for `ElixirRs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirRs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_rs_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirRsTest do
+  use ExUnit.Case
+  doctest ElixirRs
+
+  test "greets the world" do
+    assert ElixirRs.hello() == :world
+  end
+end
